094871392102ab91cab36ea4f2be6096553298f9
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-07-08 16:46:06 +0700

    Update addresses.js

--- src/constants/addresses.js
+++ src/constants/addresses.js
@@ -1,20 +1,24 @@
+// src/constants/addresses.js
+
+export const SWAP_CONTRACT = "0x0c733a6cd30714e92fdfac5ef154a9c9eab5a7d2"; // contract UniversalTokenSwap kamu
+
 export const TOKENS = [
   {
     name: "Somnia Token",
     symbol: "STT",
-    address: "0x1234567890abcdef1234567890abcdef12345678", // ← alamat STT sesuai jaringan
+    address: "0x5d58A21c4351CdF41dF284E405d73F9fc19b1435", // STT di Somnia Testnet
     icon: "/stt.png",
   },
   {
     name: "Gold Token",
     symbol: "GOLD",
-    address: "0xabcdefabcdefabcdefabcdefabcdefabcdefabcd", // GOLD
+    address: "0x84dB40A1E510A15764bCff3E2F8cf72A1c9503E6", // GOLD di Somnia Testnet
     icon: "/gold.png",
   },
   {
     name: "Gem Token",
     symbol: "GEM",
-    address: "0xaabbccddeeff00112233445566778899aabbccdd", // GEM
+    address: "0xC1b4CEcE0c0507D9C5f812eFDb66F726A13F1B0B", // GEM di Somnia Testnet
     icon: "/gem.png",
   },
 ];

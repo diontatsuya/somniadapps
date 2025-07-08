f7a9c01d5c00abfce4a316a72114fb6154fde58f
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-07-08 13:45:24 +0700

    Update addresses.js

--- src/constants/addresses.js
+++ src/constants/addresses.js
@@ -1,13 +1,8 @@
+export const STT = {
+  name: "STT",
+  address: "0x0000000000000000000000000000000000000000",
+  icon: "/stt.png"
+};
+export const TOKEN_A = { /* GOLD */ };
+export const TOKEN_B = { /* GEM */ };
 export const SWAP_CONTRACT = import.meta.env.VITE_SWAP_CONTRACT;
-
-export const TOKEN_A = {
-  name: "GOLD",
-  address: "0x7e86277abbedac497e23d7abf43913833fb7ba2e",
-  icon: "/gold.png"
-};
-
-export const TOKEN_B = {
-  name: "GEM",
-  address: "0x73f75ac5400f48bad2bff033eae4248cfef9b499",
-  icon: "/gem.png"
-};

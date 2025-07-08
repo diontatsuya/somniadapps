d1ccecd99942923a95ac3e204c39cfaa04a2a652
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-07-08 15:35:21 +0700

    Update addresses.js

--- src/constants/addresses.js
+++ src/constants/addresses.js
@@ -1,22 +1,15 @@
-// src/constants/addresses.js
-export const STT = {
-  name: "STT",
-  address: "STT",
-  icon: "/stt.png"
-};
+export const SWAP_CONTRACT = import.meta.env.VITE_SWAP_CONTRACT;
 
 export const TOKEN_A = {
+  address: import.meta.env.VITE_TOKEN_A,
   name: "GOLD",
-  address: "0x7e86277abbedac497e23d7abf43913833fb7ba2e",
-  icon: "/gold.png"
+  symbol: "GLD",
+  icon: "/gold.png",
 };
 
 export const TOKEN_B = {
+  address: import.meta.env.VITE_TOKEN_B,
   name: "GEM",
-  address: "0x73f75ac5400f48bad2bff033eae4248cfef9b499",
-  icon: "/gem.png"
+  symbol: "GEM",
+  icon: "/gem.png",
 };
-
-export const SWAP_CONTRACT = import.meta.env.VITE_SWAP_CONTRACT;
-
-export const TOKEN_LIST = [STT, TOKEN_A, TOKEN_B];

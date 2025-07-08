b5f7ffb5319581295a195ab29c3debad37a31a58
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-07-08 15:37:30 +0700

    Update universalTokenSwapAbi.js

--- src/abi/universalTokenSwapAbi.js
+++ src/abi/universalTokenSwapAbi.js
@@ -1,8 +1,8 @@
 export const universalTokenSwapAbi = [
   {
     "inputs": [
-      { "internalType": "address", "name": "from", "type": "address" },
-      { "internalType": "address", "name": "to", "type": "address" },
+      { "internalType": "address", "name": "fromToken", "type": "address" },
+      { "internalType": "address", "name": "toToken", "type": "address" },
       { "internalType": "uint256", "name": "amount", "type": "uint256" }
     ],
     "name": "swap",
@@ -12,53 +12,11 @@ export const universalTokenSwapAbi = [
   },
   {
     "inputs": [
-      { "internalType": "address", "name": "from", "type": "address" },
-      { "internalType": "address", "name": "to", "type": "address" },
+      { "internalType": "address", "name": "fromToken", "type": "address" },
+      { "internalType": "address", "name": "toToken", "type": "address" },
       { "internalType": "uint256", "name": "amount", "type": "uint256" }
     ],
     "name": "estimateSwap",
-    "outputs": [
-      { "internalType": "uint256", "name": "", "type": "uint256" }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      { "internalType": "address", "name": "from", "type": "address" },
-      { "internalType": "address", "name": "to", "type": "address" },
-      { "internalType": "uint256", "name": "rate", "type": "uint256" }
-    ],
-    "name": "setRate",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      { "internalType": "address", "name": "token", "type": "address" },
-      { "internalType": "uint256", "name": "amount", "type": "uint256" }
-    ],
-    "name": "withdrawToken",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  },
-  {
-    "inputs": [],
-    "name": "owner",
-    "outputs": [
-      { "internalType": "address", "name": "", "type": "address" }
-    ],
-    "stateMutability": "view",
-    "type": "function"
-  },
-  {
-    "inputs": [
-      { "internalType": "address", "name": "from", "type": "address" },
-      { "internalType": "address", "name": "to", "type": "address" }
-    ],
-    "name": "rates",
     "outputs": [
       { "internalType": "uint256", "name": "", "type": "uint256" }
     ],

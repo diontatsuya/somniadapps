2df1e078c944e7c640d37203430005a67f30c4b3
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-07-11 21:31:45 +0700

    Update addresses.js

--- src/constants/addresses.js
+++ src/constants/addresses.js
@@ -4,6 +4,12 @@ export const SWAP_CONTRACT = "0x63a04beb918655679123c2dc1f43d142da5c7fea";
 
 export const TOKENS = [
   {
+    name: "Somnia Test Token",
+    symbol: "STT",
+    address: "0x0000000000000000000000000000000000000000", // STT = native token
+    icon: "/stt.png",
+  },
+  {
     name: "Gold Token",
     symbol: "GOLD",
     address: "0x7e86277abbedac497e23d7abf43913833fb7ba2e",
@@ -14,11 +20,5 @@ export const TOKENS = [
     symbol: "GEM",
     address: "0x73f75ac5400f48bad2bff033eae4248cfef9b499",
     icon: "/gem.png",
-  },
-  {
-    name: "Somnia Test Token",
-    symbol: "STT",
-    address: "0x7f89af8b3c0A68F536Ff20433927F4573CF001A3", // alamat STT resmi
-    icon: "/stt.png",
   },
 ];

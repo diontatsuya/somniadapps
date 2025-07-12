fc21225cc2ba3cd6a8a84de77178eb605914d157
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-07-12 10:33:49 +0700

    Update universalTokenSwapAbi.js

--- src/abi/universalTokenSwapAbi.js
+++ src/abi/universalTokenSwapAbi.js
@@ -1,26 +1,24 @@
 export const universalTokenSwapAbi = [
   {
-    "inputs": [
-      { "internalType": "address", "name": "fromToken", "type": "address" },
-      { "internalType": "address", "name": "toToken", "type": "address" },
-      { "internalType": "uint256", "name": "amount", "type": "uint256" }
+    inputs: [
+      { internalType: "address", name: "fromToken", type: "address" },
+      { internalType: "address", name: "toToken", type: "address" },
+      { internalType: "uint256", name: "amount", type: "uint256" }
     ],
-    "name": "swap",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
+    name: "swap",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function"
   },
   {
-    "inputs": [
-      { "internalType": "address", "name": "fromToken", "type": "address" },
-      { "internalType": "address", "name": "toToken", "type": "address" },
-      { "internalType": "uint256", "name": "amount", "type": "uint256" }
+    inputs: [
+      { internalType: "address", name: "", type: "address" },
+      { internalType: "address", name: "", type: "address" },
+      { internalType: "uint256", name: "", type: "uint256" }
     ],
-    "name": "estimateSwap",
-    "outputs": [
-      { "internalType": "uint256", "name": "", "type": "uint256" }
-    ],
-    "stateMutability": "view",
-    "type": "function"
+    name: "estimateSwap",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "pure",
+    type: "function"
   }
 ];

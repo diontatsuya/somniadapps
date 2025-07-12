dc893f9e312cd347bb2d7c9f03e949869fdddae9
diontatsuya03 <44664668+diontatsuya@users.noreply.github.com>
2025-07-12 10:38:55 +0700

    Update addresses.js

--- src/constants/addresses.js
+++ src/constants/addresses.js
@@ -1,22 +1,22 @@
-export const SWAP_CONTRACT = "0x63a04beb918655679123c2dc1f43d142da5c7fea";
+export const SWAP_CONTRACT = "0x9b82b49fa72a16f078c4ff8cf3243e71ca5b6ff1";
 
 export const TOKENS = [
   {
     name: "Gold Token",
     symbol: "GOLD",
-    address: "0x7e86277abbedac497e23d7abf43913833fb7ba2e".toLowerCase(),
+    address: "0x7e86277abbedac497e23d7abf43913833fb7ba2e",
     icon: "/gold.png",
   },
   {
     name: "Gem Token",
     symbol: "GEM",
-    address: "0x73f75ac5400f48bad2bff033eae4248cfef9b499".toLowerCase(),
+    address: "0x73f75ac5400f48bad2bff033eae4248cfef9b499",
     icon: "/gem.png",
   },
   {
     name: "Somnia Test Token",
     symbol: "STT",
-    address: "0x7f89af8b3c0a68f536ff20433927f4573cf001a3".toLowerCase(),
+    address: "0x7f89af8b3c0a68f536ff20433927f4573cf001a3",
     icon: "/stt.png",
   },
 ];
